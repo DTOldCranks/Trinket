f14d6a513c2e24f2da8b24258621b64a6eca74ae
melse2 <meilin.else@gmail.com>
2016-03-30 17:18:42 -0400

    Update send_to_PC.ino

--- send_to_PC.ino
+++ send_to_PC.ino
@@ -3,7 +3,8 @@ int counter = 0;
 
 #define RXDATA    0      // SoftwareSerial receive pin.
 #define TXDATA    1      // SoftwareSerial transmit pin.
-#define ANASG     1      // Analog channel 1 for strain gauge measurement
+#define ANASG1     1      // Analog channel 1 for strain gauge measurement
+#define ANASG3     3      // Analog channel 3 for strain gauge measurement
 
 SoftwareSerial serial(RXDATA, TXDATA);
 
@@ -13,9 +14,11 @@ void setup() {
 
 void loop() {
   serial.print("Voltage: ");
-  int sensorValue = analogRead(ANASG);
-  float voltage = sensorValue * (5.0 / 1023.0 * 1000.0);
-  serial.print(voltage);
+  int sensorValue1 = analogRead(ANASG1);
+  int sensorValue3 = analogRead(ANASG3);
+  float voltage1 = sensorValue1 * (5.0 / 1023.0 * 1000.0); // change to bits and convert to mV
+  float voltage3 = sensorValue3 * (5.0 / 1023.0 * 1000.0);
+  serial.print(abs(voltage1-voltage3));
   serial.println(" mV");
   delay(100); // wait half a sec
 }
