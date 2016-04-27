40ba73daa31cd7cbb5a252eb94f1d09acb285da6
Sleeepybear1 <gehn.ferguson@gmail.com>
2016-04-27 18:58:55 -0400

    Added files via upload

---
+++ sketch_apr27b.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+SoftwareSerial serial(1, 4);
+Adafruit_ADS1015 ads;
+void setup() {
+  // put your setup code here, to run once:
+  serial.begin(9600);
+  ads.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int16_t results;
+
+  float multiplier = 3.0F;
+  results = ads.readADC_Differential_0_1();
+  serial.print(results); serial.print("("); serial.print(results * multiplier); serial.println("mV)");
+  delay(1000);
+}
